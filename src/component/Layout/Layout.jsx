ca972fbfea687d57741cfb52b445f9abedaf7902
mohannad-shaaban <mohannadshaaban2322005@gmail.com>
2025-06-22 15:59:28 +0300

    Initial commit

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+<<<<<<< HEAD
 import Navbar from '../Navbar/Navbar'
+=======
+import NAvbar from '../Navbar/NAvbar'
+>>>>>>> 0d28195 (Initial commit)
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
     return (
         <div className="flex flex-col min-h-screen">
+<<<<<<< HEAD
         <Navbar />
+=======
+        <NAvbar />
+>>>>>>> 0d28195 (Initial commit)
         <div className="flex-grow"> 
             <Outlet />
         </div>
