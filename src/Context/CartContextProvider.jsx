266ec29d13ecaf9e2242fb20604431bf00313a08
mohannad-shaaban <mohannadshaaban2322005@gmail.com>
2026-05-12 05:43:12 +0300

    update cart context

--- src/Context/CartContextProvider.jsx
+++ src/Context/CartContextProvider.jsx
@@ -2,44 +2,54 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import { createContext } from 'react'
+import { createContext, useContext } from 'react'
+import { AuthContext } from './AuthContextProvider'
 export let CartContext = createContext()
 export default function CartContextProvider({ children }) {
-    let [numsCartItem, setNumsCartItem] = useState(null)
+    let [numsCartItem, setNumsCartItem] = useState(0)
+    let { token } = useContext(AuthContext)
     const baseUrl = 'https://ecommerce.routemisr.com/api/v1/cart'
-    const headerOptions = {
-        headers: {
-            token: localStorage.getItem("token"),
-        }
-    };
+
+    function getHeaderOptions() {
+        return {
+            headers: {
+                token: token || localStorage.getItem("token"),
+            }
+        };
+    }
+
     useEffect(() => {
-        if (localStorage.getItem("token")) {
+        if (token || localStorage.getItem("token")) {
             getUserCart().then((req) => {
-                console.log(req.data.numOfCartItems);
                 setNumsCartItem(req.data.numOfCartItems);
+            }).catch(() => {
+                setNumsCartItem(0)
             })
+        } else {
+            setNumsCartItem(0)
         }
-    })
+    }, [token])
+
     function getUserCart() {
-        return axios.get(baseUrl, headerOptions);
+        return axios.get(baseUrl, getHeaderOptions());
     }
     function addUserCart(id) {
         let data = {
             productId: id
         }
-        return axios.post(baseUrl, data, headerOptions)
+        return axios.post(baseUrl, data, getHeaderOptions())
     }
     function deleteUserCart(id) {
-        return axios.delete(`${baseUrl}/${id}`, headerOptions)
+        return axios.delete(`${baseUrl}/${id}`, getHeaderOptions())
     }
     function clearUserCart() {
-        return axios.delete(baseUrl, headerOptions)
+        return axios.delete(baseUrl, getHeaderOptions())
     }
     function updateCart(id, count) {
         let data = {
             count: count
         }
-        return axios.put(`${baseUrl}/${id}`, data, headerOptions)
+        return axios.put(`${baseUrl}/${id}`, data, getHeaderOptions())
     }
     return (
         <CartContext.Provider value={{ getUserCart, numsCartItem, setNumsCartItem, addUserCart, deleteUserCart, clearUserCart, updateCart }}>{children}</CartContext.Provider>
